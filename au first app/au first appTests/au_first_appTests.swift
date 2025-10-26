4043dc6b4f90f11ad300578c5b73ba9ada93a9e0
lsteven816 <142756833+lsteven816@users.noreply.github.com>
2025-10-26 13:05:25 -0400

    Add files via upload
    Added first app files

---
+++ au first app/au first appTests/au_first_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  au_first_appTests.swift
+//  au first appTests
+//
+//  Created by LaToya Stevens on 10/26/25.
+//
+
+import Testing
+@testable import au_first_app
+
+struct au_first_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
